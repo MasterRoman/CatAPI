afdc4383021061d06cd90d89601f15fd25c11442
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-05 16:44:38 +0300

    configure main tab bar

--- CatAPI/View/MainScreen/MainTabBarController.swift
+++ CatAPI/View/MainScreen/MainTabBarController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    private var catViewController : CatViewController?
+    private var uploadViewController : UploadViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,20 @@ class MainTabBarController: UITabBarController {
     }
     
     private func setUpView(){
+        
+        self.catViewController = CatViewController.init()
+        let catTab : UITabBarItem = UITabBarItem.init(title: nil, image: UIImage.init(named: "cat_unselected"), selectedImage: UIImage.init(named: "cat_selected"))
+        self.catViewController?.tabBarItem = catTab
+        let catNavigationController : UINavigationController = UINavigationController.init(rootViewController: self.catViewController!)
+        
+        self.uploadViewController = UploadViewController.init()
+        let uploadTab : UITabBarItem = UITabBarItem.init(title: nil, image: UIImage.init(named: "upload_unselected"), selectedImage: UIImage.init(named: "upload_selected"))
+        self.uploadViewController?.tabBarItem = uploadTab
+        let uploadNavigationController : UINavigationController = UINavigationController.init(rootViewController: self.uploadViewController!)
+        
+        
+        self.viewControllers = [catNavigationController,uploadNavigationController]
+        self.selectedIndex = 0
         
     }
 
