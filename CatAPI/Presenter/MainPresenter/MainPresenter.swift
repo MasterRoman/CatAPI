e57515cb5b6b461883a0d44a8efa9ecdb92e3a15
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-05 15:46:02 +0300

    add main presenter

---
+++ CatAPI/Presenter/MainPresenter/MainPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MainPresenter.swift
+//  CatAPI
+//
+//  Created by Admin on 05.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import UIKit
+
+class MainPresenter: NSObject {
+
+}
