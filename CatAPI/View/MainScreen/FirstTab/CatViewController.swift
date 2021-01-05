d588597b1cd40afba68a1ee5891b330a9ac9726c
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-05 16:13:04 +0300

    add cat tab

---
+++ CatAPI/View/MainScreen/FirstTab/CatViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CatViewController.swift
+//  CatAPI
+//
+//  Created by Admin on 05.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import UIKit
+
+class CatViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,CatViewDelegateProtocol {
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : UICollectionViewCell = UICollectionViewCell.init()
+        return cell
+    }
+    
+    
+}
---
+++ CatAPI/View/MainScreen/MainTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabBarController.swift
+//  CatAPI
+//
+//  Created by Admin on 05.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setUpView()
+    }
+    
+    private func setUpView(){
+        
+    }
+
+}
