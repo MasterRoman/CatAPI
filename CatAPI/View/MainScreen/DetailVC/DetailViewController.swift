5d160acb5267bc7840afb8a947807dda28d211fd
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-09 18:05:11 +0300

    implement delegate methods

--- CatAPI/View/MainScreen/DetailVC/DetailViewController.swift
+++ CatAPI/View/MainScreen/DetailVC/DetailViewController.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,DetailViewDelegateProtocol {
+    var imageView: UIImageView?
+    
+    var url: String?
+    
+    var saveButton: UIButton?
+    
+    required init(with image: UIImage, url: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.imageView = UIImageView.init(image: image)
+        self.url = url
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func showSavedStatusAlert() {
+        let alert = UIAlertController.init(title: "Save", message: "Saved!", preferredStyle: .alert)
+        let action = UIAlertAction.init(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
