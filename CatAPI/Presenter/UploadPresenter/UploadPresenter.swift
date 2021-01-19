fba5916fd99396c31bc7436b5dd1bb35bbfa17d8
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-19 18:31:41 +0300

    add basic func to upload presenter

--- CatAPI/Presenter/UploadPresenter/UploadPresenter.swift
+++ CatAPI/Presenter/UploadPresenter/UploadPresenter.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class UploadPresenter: NSObject {
-
+    
+    private var uploadDelegate : UploadViewDelegateProtocol?
+    
+    private var networkManeger : NetworkManager?
+    
+    private var userManeger : UserManager?
+    
+    private var parser : JSONParser?
+    private var catsArray : Array<CatModel>?
+    
+    override init() {
+        self.parser = JSONParser.init()
+        self.networkManeger = NetworkManager.init(withParser: self.parser!)
+        self.userManeger = UserManager.init()
+        self.catsArray = Array<CatModel>.init()
+    }
+    
+    func setUploadViewDelegate(view : UploadViewDelegateProtocol){
+        self.uploadDelegate = view
+    }
+    
+    func registerCell(for collectionView:UICollectionView){
+        collectionView.register(CatCell.classForCoder(), forCellWithReuseIdentifier: "CellId")
+//        collectionView.register(UICollectionReusableView.classForCoder(), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "Footer")
+    }
+    
+    
+    
+    
 }
