3476a19e1c45d61c63821ad871b3fc3ad4de6a22
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-19 17:58:47 +0300

    add upload presenter

---
+++ CatAPI/Presenter/UploadPresenter/UploadPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  UploadPresenter.swift
+//  CatAPI
+//
+//  Created by Admin on 19.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import UIKit
+
+class UploadPresenter: NSObject {
+
+}
