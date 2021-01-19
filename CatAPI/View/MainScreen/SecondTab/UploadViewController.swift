9158d36d1f1e5fa02852a7186add0865b727bbea
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-19 17:45:15 +0300

    add functions to VC

--- CatAPI/View/MainScreen/SecondTab/UploadViewController.swift
+++ CatAPI/View/MainScreen/SecondTab/UploadViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class UploadViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UploadViewDelegateProtocol {
+
+    var collectionView: UICollectionView!
+    var layout: UICollectionViewFlowLayout!
+    
+  //  private var presenter : UploadPresenter?
+    
+    private var catsSource : Array<CatModel>?
+    
+    private var indicator : UIActivityIndicatorView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +25,8 @@ class UploadViewController: UIViewController,UICollectionViewDelegate,UICollecti
         // Do any additional setup after loading the view.
     }
     
+    
+    //MARK: Collection view dataSorce methods 
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 0
@@ -25,5 +36,40 @@ class UploadViewController: UIViewController,UICollectionViewDelegate,UICollecti
         let cell : UICollectionViewCell = UICollectionViewCell.init()
         return cell
     }
+    
+    //MARK: Delegate methods
+    
+    func showCats(array: Array<CatModel>) {
+        
+    }
+    
+    func addMoreImages(array: Array<CatModel>) {
+            
+    }
+    
+    func checkUserRegistration() {
+        
+    }
+    
+    func showAlertController() {
+        
+    }
+    
+    func showErrorAlert(error: Error?) {
+        
+    }
+    
+    func showAuthErrorAlert() {
+        
+    }
+    
+    func startIndicator() {
+        
+    }
+    
+    func stopIndicator() {
+        
+    }
+    
     
 }
