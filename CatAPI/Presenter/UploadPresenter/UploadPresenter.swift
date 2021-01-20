cdc2a2da13ecb0968e589625071e0cbec2114f46
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-20 19:21:29 +0300

    add delegate

--- CatAPI/Presenter/UploadPresenter/UploadPresenter.swift
+++ CatAPI/Presenter/UploadPresenter/UploadPresenter.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UploadPresenter: NSObject {
+class UploadPresenter: NSObject,UIImagePickerControllerDelegate {
     
     private var uploadDelegate : UploadViewDelegateProtocol?
     
