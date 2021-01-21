a5650f9df4361ef568bea8e7441b8d286499d57a
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-21 18:45:32 +0300

    add profile files

---
+++ CatAPI/Presenter/ProfilePresenter/ProfilePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  ProfilePresenter.swift
+//  CatAPI
+//
+//  Created by Admin on 21.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePresenter: NSObject {
+
+}
---
+++ CatAPI/View/MainScreen/ThirdTab/ProfileDelegateProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileDelegateProtocol.swift
+//  CatAPI
+//
+//  Created by Admin on 21.01.2021.
+//  Copyright © 2021 MasterCORP. All rights reserved.
+//
+
+import Foundation
