c65987e205464adecfa5d067f48fc4c7445a65b9
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-28 19:51:58 +0300

    test catModel

--- CatAPITests/CatModelTest.swift
+++ CatAPITests/CatModelTest.swift
@@ -16,7 +16,6 @@ class CatModelTest: XCTestCase {
     
     override func setUpWithError() throws {
         self.dataSource = ["id":"1","url":"2"]
-        self.item = CatModel.init(withDictionary: self.dataSource!)
     }
 
     override func tearDownWithError() throws {
@@ -24,9 +23,62 @@ class CatModelTest: XCTestCase {
         self.item = nil
     }
 
-    func testExample() throws {
-        
+    func testInitializationFromDictionary() throws {
+        self.item = CatModel.init(withDictionary: self.dataSource!)
+        XCTAssertEqual(self.item?.imageId, "1")
+        XCTAssertEqual(self.item?.url, "2")
     }
 
+    func testInitializationFromUrl() throws {
+        self.item = CatModel.init(with: "2")
+        XCTAssertEqual(self.item?.imageId, "")
+        XCTAssertEqual(self.item?.url, "2")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
