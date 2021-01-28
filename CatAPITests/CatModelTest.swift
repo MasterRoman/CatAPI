43383e074a621fdf5521c10321a4ef1db2a39a12
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-28 19:44:38 +0300

    add tests

--- CatAPITests/CatModelTest.swift
+++ CatAPITests/CatModelTest.swift
@@ -7,27 +7,26 @@
 //
 
 import XCTest
+@testable import CatAPI
 
 class CatModelTest: XCTestCase {
 
+    private var dataSource : Dictionary<String, Any>?
+    private var item : CatModel?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.dataSource = ["id":"1","url":"2"]
+        self.item = CatModel.init(withDictionary: self.dataSource!)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+         self.dataSource = nil
+        self.item = nil
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
 }
