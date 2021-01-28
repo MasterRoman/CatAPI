cdc591b97e7787d4d1ebaa796d00da61dda635e5
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-28 22:30:26 +0300

    add modifications

--- CatAPI/Services/JSONParser.swift
+++ CatAPI/Services/JSONParser.swift
@@ -39,7 +39,7 @@ class JSONParser: NSObject {
         var cats : Array<CatModel> = []
         if let dict = rootDictionary{
             for item in dict{
-                cats.append(CatModel.init(withDictionary: item))
+                cats.append(CatModel.init(withDictionary: item as Dictionary))
             }
         }
         completion(.success(cats))
