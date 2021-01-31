bcb480d72253227842cca18d35dff58231f60e24
Roman Kozko <romanchek_bananchek@mail.ru>
2021-01-31 17:38:21 +0300

    cover by tests all network manager

--- CatAPITests/NetworkManagerTest.swift
+++ CatAPITests/NetworkManagerTest.swift
@@ -66,26 +66,43 @@ class NetworkManagerTest: XCTestCase {
         self.networkManager?.loadImageForUrl(url: filePath, completion: { (image) in
             XCTAssertEqual(localImage, image)
         })
+        
+        self.networkManager?.loadImageForUrl(url: filePath, completion: { (image) in
+            XCTAssertEqual(localImage, image)
+        })
+        
     }
     
     func testChaching() throws {
         let localImage : UIImage = UIImage.init(named: "cat")!
-        let filePath : String = Bundle(for: type(of: self)).path(forResource: "cat.png", ofType: nil)!
+        let catImage : UIImage = UIImage.init(named: "cat_selected")!
+        let filePath : String = Bundle(for: type(of: self)).path(forResource: "animal-footprint.png", ofType: nil)!
+        self.networkManager = NetworkManagerMock.init(withParser: self.parser!)
+        
+        var numberOfReceiving = 0
+        
         self.networkManager?.getChachedImage(for: filePath, completion: { result in
             switch result{
             case .success(let image):
-                XCTAssertEqual(localImage, image)
+                if (numberOfReceiving == 0){
+                    XCTAssertEqual(localImage, image)   //receive placeholder
+                    numberOfReceiving += 1
+                }
+                else {
+                    XCTAssertEqual(catImage, image)  // necessary image
+                }
+                
             case .failure(_):
                 break
             }
             
         })
         
-        //for canceling 
+        //get from chache from chache without downloading
         self.networkManager?.getChachedImage(for: filePath, completion: { result in
             switch result{
             case .success(let image):
-                XCTAssertEqual(localImage, image)
+                XCTAssertEqual(catImage, image)
             case .failure(_):
                 break
             }
@@ -94,6 +111,136 @@ class NetworkManagerTest: XCTestCase {
     }
     
     
-  
-
+    func testLoadingUplodedCats() throws {
+        
+        let filePath = Bundle(for: type(of: self)).path(forResource: "MockJson", ofType: nil)
+        let url = "https://Someurl"
+        let apiKey = "Some api"
+        
+        let mockData = NSData.init(contentsOfFile: filePath!)
+        self.mockSession?.data = mockData as Data?
+        self.networkManager?.session = self.mockSession!
+        
+        self.networkManager?.loadUplodedCats(for: url, apiKey: apiKey, completion: { result in
+            switch result{
+            case .success(let array):
+                XCTAssertTrue(array.count == 2)
+            case .failure(_):
+                break
+            }
+            
+        })
+        
+        let mockError = NSError(domain: "mock", code: 0, userInfo: nil)
+        self.mockSession?.error = mockError as Error
+        
+        self.networkManager?.loadUplodedCats(for: url, apiKey: apiKey, completion: { result in
+            switch result{
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTAssertNotNil(error.self.localizedDescription)
+            }
+            
+        })
+        
+       
+        
+        self.mockSession?.data = nil
+        self.mockSession?.error = nil
+        //exit because of absense of date 
+        self.networkManager?.loadUplodedCats(for: url, apiKey: apiKey, completion: { _ in
+    
+        })
+    }
+    
+    
+    func testUploadingImage() throws{
+        
+        let filePath = Bundle(for: type(of: self)).path(forResource: "MockJsonForDictionary", ofType: nil)
+        let url = "https://Someurl"
+        let fileName = "Somefile"
+        let image = UIImage.init(named: "cat")
+        
+        let mockString = try String(contentsOfFile: filePath!)
+        let mockData : Data = mockString.data(using: .utf8)!
+        
+        self.mockSession?.data = mockData 
+        self.networkManager?.session = self.mockSession!
+        
+        self.networkManager?.uploadImage(for: url, fileName: fileName, image: image!, completion: { result in
+            switch result{
+            case .success(let dict):
+                XCTAssertEqual(dict["url"] as! String, "https://cdn2.thecatapi.com/images/MTYxMjc1OQ.jpg")
+            case .failure(_):
+                break
+            }
+            
+        })
+        
+        self.mockSession?.data = Data.init()
+        
+        self.networkManager?.uploadImage(for: url, fileName: fileName, image: image!, completion: { result in
+            switch result{
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTAssertNotNil(error.self.localizedDescription)
+            }
+            
+        })
+        
+        let mockError = NSError(domain: "mock", code: 0, userInfo: nil)
+        self.mockSession?.error = mockError as Error
+        
+        self.networkManager?.uploadImage(for: url, fileName: fileName, image: image!, completion: { result in
+            switch result{
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTAssertNotNil(error.self.localizedDescription)
+            }
+            
+        })
+        
+    }
+    
+    func testDeletingUplodedImageFromServer() throws{
+        
+        let imageId = "SomeId"
+        let apiKey = "Some api"
+        
+        self.networkManager?.session = self.mockSession!
+        
+        let promise = expectation(description: "handeler")
+        self.networkManager?.deleteUplodedImageFromServer(for: apiKey, imageId: imageId, completion: { result in
+            switch result{
+            case .success(let str):
+                XCTAssertEqual(str, "Deleted")
+                promise.fulfill()
+            case .failure(_):
+                break
+            }
+            
+        })
+        waitForExpectations(timeout: 5.0, handler: nil)
+        
+        let mockError = NSError(domain: "mock", code: 0, userInfo: nil)
+        self.mockSession?.error = mockError as Error
+        
+        //error
+        self.networkManager?.deleteUplodedImageFromServer(for: apiKey, imageId: imageId, completion: { result in
+            switch result{
+            case .success(_):
+                break
+            case .failure(let error):
+                XCTAssertNotNil(error.self.localizedDescription)
+            }
+            
+        })
+    }
+    
+    
 }
+
+
